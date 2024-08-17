8186016136f8419fcea745dbf7bc211cf8685812
f-prototype <lehasazonoff@mail.ru>
2024-08-17 15:11:47 +0300

    lab2

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('main/hello');
-});
+Route::get('/', [MainController::class, 'index']);
+Route::get('/galery/{full_image}', [MainController::class, 'show']);
 Route::get('/about', function () {
     return view('main/about');
 });
